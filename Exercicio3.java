e2e4e369a6581988584a3ff763c285aebc1c7dae
Luxiosis <lucasdudu1001@gmail.com>
2022-11-16 15:42:02 -0300

    lista de exrecicios 2 java

--- Exercicio3.java
+++ Exercicio3.java
@@ -13,7 +13,8 @@ public class Exercicio3 {
         System.out.println("Digite sua renda mensal:");
         double renda = sc.nextDouble();
 
-        double emprestimo_mensal = emprestimo / parcelas;
+        double unica_parcela = emprestimo / parcelas;
+        double trinta_porcento = (renda / 100) * 30;
 
         if ((idade < 18 || idade > 65)) {
             System.out.printf("A pessoa que está solicitando o empréstimo tem %.0f anos de idade e tem o salário de %.1f reais.\n", idade, renda);
@@ -23,7 +24,7 @@ public class Exercicio3 {
             System.out.printf("A pessoa que está solicitando o empréstimo tem %.0f anos de idade e tem o salário de %.1f reais.\n", idade, renda);
             System.out.printf("O valor solicitado é de %.1f reais para ser pago em %.0f parcelas.\n", emprestimo, parcelas);
             System.out.println("O empréstimo foi reprovado");
-        } else if (renda < emprestimo_mensal){
+        } else if (trinta_porcento < unica_parcela){
             System.out.printf("A pessoa que está solicitando o empréstimo tem %.0f anos de idade e tem o salário de %.1f reais.\n", idade, renda);
             System.out.printf("O valor solicitado é de %.1f reais para ser pago em %.0f parcelas.\n", emprestimo, parcelas);
             System.out.println("O empréstimo foi reprovado");
